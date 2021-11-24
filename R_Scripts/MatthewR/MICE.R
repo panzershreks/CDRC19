e9b500ee9f2a8fc2fcac189ce0a98fe5aca69202
MattthewD <66824293+MattthewD@users.noreply.github.com>
2021-11-24 11:56:02 +0000

    MD

--- R_Scripts/MatthewR/MICE.R
+++ R_Scripts/MatthewR/MICE.R
@@ -2,7 +2,8 @@
 library('mice')
 library(tidyverse)
 library(dplyr)
-
+library("janitor")
+library(ggplot2)
 
 # Create a pretend dataset
 Age <- c(12,14,15,16,17,12,15,16,17,18,19,19,18,16,17,18)
@@ -39,6 +40,12 @@ fully_merged_df <- read_csv("Combined DataFrame Work/fully_merged_df.csv")
 
 fully_merged_df <- subset(fully_merged_df, select = -1)
 
+# The MICE function does not like having spaces in the column name,
+# so we use this function to replace all of the spaces with _
+
+fully_merged_df <- clean_names(fully_merged_df)
+?step
+
 test_mean_df <- subset(fully_merged_df, select = c(1,13,54,58))
 test_mice_df <- subset(fully_merged_df, select = c(1,13,54,58))
 
@@ -48,11 +55,44 @@ summary(test_mice_df)
 # Now we do mean imputation
 
 test_mean_df$stringency_index[which(is.na(test_mean_df$stringency_index))] = mean(test_mean_df$stringency_index, na.rm = TRUE)
-test_mean_df$`Total confirmed deaths due to COVID-19`[which(is.na(test_mean_df$`Total confirmed deaths due to COVID-19`))] = mean(test_mean_df$`Total confirmed deaths due to COVID-19`, na.rm = TRUE)
-test_mean_df$`Total confirmed deaths due to COVID-19 per million people`[which(is.na(test_mean_df$`Total confirmed deaths due to COVID-19 per million people`))] = mean(test_mean_df$`Total confirmed deaths due to COVID-19 per million people`, na.rm = TRUE)
+test_mean_df$total_confirmed_deaths_due_to_covid_19[which(is.na(test_mean_df$total_confirmed_deaths_due_to_covid_19))] = mean(test_mean_df$total_confirmed_deaths_due_to_covid_19, na.rm = TRUE)
+test_mean_df$total_confirmed_deaths_due_to_covid_19_per_million_people[which(is.na(test_mean_df$total_confirmed_deaths_due_to_covid_19_per_million_people))] = mean(test_mean_df$total_confirmed_deaths_due_to_covid_19_per_million_people, na.rm = TRUE)
 
 
-# Now to try MICE - but this doesn't work...
+# Now to try MICE
 
-covid_imputation <- mice(data = test_mice_df, m = 5, method = c("","pmm","pmm","pmm"), maxit = 20)
+covid_imputation <- mice(data = test_mice_df, m = 5, method = c("","pmm","pmm","pmm"), maxit = 100)
+
+?mice
+
+covid_imputation$imp$stringency_index
+
+
+
+covid_mice_df1 <- complete(covid_imputation, 1)
+covid_mice_df2 <- complete(covid_imputation, 2)
+covid_mice_df3 <- complete(covid_imputation, 3)
+covid_mice_df4 <- complete(covid_imputation, 4)
+covid_mice_df5 <- complete(covid_imputation, 5)
+
+
+ggplot(data = covid_mice_df1, aes(x=total_confirmed_deaths_due_to_covid_19_per_million_people, y = stringency_index)) + geom_point() + geom_smooth(method="lm")
+
+# I think at this stage we then take all five imputed datasets and make models off of them
+# and then essentially work with these and gather thoughts based on all of the models.
+
+#https://stats.idre.ucla.edu/r/faq/how-do-i-perform-multiple-imputation-using-predictive-mean-matching-in-r/
+
+# Let's try this for modelling the death rates per populatiuon against the stringency index.
+
+fitm <- with(covid_imputation, lm(total_confirmed_deaths_due_to_covid_19_per_million_people ~ stringency_index  ))
+
+summary(fitm)
+
+summary(pool(fitm))
+
+# this shows that these things aren't very correlated/there is not a significant relationship.
+
+
+
 
