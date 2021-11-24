6eb8e3ef0b66a35f051fab33a757c9fae3deb3a4
MattthewD <66824293+MattthewD@users.noreply.github.com>
2021-11-24 17:24:55 +0000

    MD Mice

---
+++ R_Scripts/MatthewR/Factor Variables.R
@@ -0,0 +1,50 @@
+# Here we define the factor variables.
+
+fully_merged_df
+fully_merged_df <- subset(fully_merged_df, select = -1)
+fully_merged_df <- clean_names(fully_merged_df)
+
+
+# Here, we code in the factor variables.
+
+fully_merged_df$school_closures <- as.factor(fully_merged_df$school_closures)
+
+fully_merged_df$workplace_closures <- as.factor(fully_merged_df$workplace_closures)
+
+fully_merged_df$cancel_public_events <- as.factor(fully_merged_df$cancel_public_events)
+
+fully_merged_df$close_public_transport <- as.factor(fully_merged_df$close_public_transport)
+
+fully_merged_df$public_information_campaigns <- as.factor(fully_merged_df$public_information_campaigns)
+
+fully_merged_df$restrictions_internal_movements <- as.factor(fully_merged_df$restrictions_internal_movements)
+
+fully_merged_df$international_travel_controls <- as.factor(fully_merged_df$international_travel_controls)
+
+fully_merged_df$fiscal_measures <- as.factor(fully_merged_df$fiscal_measures)
+
+fully_merged_df$emergency_investment_healthcare <- as.factor(fully_merged_df$emergency_investment_healthcare)
+
+fully_merged_df$investment_vaccines <- as.factor(fully_merged_df$investment_vaccines)
+
+fully_merged_df$contact_tracing <- as.factor(fully_merged_df$contact_tracing)
+
+fully_merged_df$restriction_gatherings <- as.factor(fully_merged_df$restriction_gatherings)
+
+fully_merged_df$stay_home_requirements <- as.factor(fully_merged_df$stay_home_requirements)
+
+fully_merged_df$income_support <- as.factor(fully_merged_df$income_support)
+
+fully_merged_df$debt_relief <- as.factor(fully_merged_df$debt_relief)
+
+fully_merged_df$international_support <- as.factor(fully_merged_df$international_support)
+
+fully_merged_df$testing_policy <- as.factor(fully_merged_df$testing_policy)
+
+fully_merged_df$facial_coverings <- as.factor(fully_merged_df$facial_coverings)
+
+fully_merged_df$vaccination_policy <- as.factor(fully_merged_df$vaccination_policy)
+
+fully_merged_df$vaccine_eligibility <- as.factor(fully_merged_df$vaccine_eligibility )
+
+
---
+++ R_Scripts/MatthewR/Mice/Numeric Covid Mice.R
@@ -0,0 +1,34 @@
+# Imputing all the numeric variables MICE.
+
+library("ggplot2")
+library("tidyverse")
+library("readr")
+library(dplyr)
+library(naniar)
+library(car)
+library(visdat)
+
+# Run the factor variable code first to make sure we have the factors!
+
+numeric_covid_data <- subset(fully_merged_df, select = -c(2:12, 14:19, 21:23))
+factor_covid_data <- subset(fully_merged_df, select = c(2:12, 14:19, 21:23))
+
+# We are now going to use MICE to imputate the numeric variables to complete it.
+
+numeric_imp <- mice(data = numeric_covid_data, m = 1, method = "cart", maxit = 5)
+
+
+# with maxit = 3 it imputed data to the first three columns, then with maxit = 5 it did the
+# same but for the first five columns. For five columns this took about an hour, so 
+# not really sure how we're going to manage to do this for 352 columns? 
+# Ask Simon about this.
+
+test_big_imp_df <- complete(numeric_imp, 1)
+
+write.csv(test_big_imp_df,"imp_maxit_5.csv", row.names = TRUE)
+
+
+
+
+missing_data_summary <- miss_var_summary(test_big_imp_df)
+missing_data_summary
