b6654054955ec7e4eb85cc9545ea423381adf21a
MattthewD <66824293+MattthewD@users.noreply.github.com>
2021-12-29 18:55:13 +0000

    MD Commit

---
+++ R_Scripts/MatthewR/Category Analysis/Food_Water/food_water_analysis.R
@@ -0,0 +1,42 @@
+library("ggplot2")
+library(grid)
+library(gridExtra)
+library(lattice)
+library("tidyverse")
+library("readr")
+library(dplyr)
+library(naniar)
+library(car)
+library(visdat)
+library("mice")
+library(UpSetR)
+library("janitor")
+library(corrplot)
+library(gt)
+
+
+clean_food_water <- read_csv("Combined DataFrame Work/CSV Files/Clean/clean_food_water.csv")
+clean_food_water <- clean_names(clean_food_water)
+clean_food_water <- subset(clean_food_water, select = -1)
+
+# We look at the missing data and export the missing data table to a CSV for making into latex.
+
+missing_table <- miss_var_summary(clean_food_water, sort_miss = TRUE)
+vis_miss(clean_food_water)
+missing_table
+
+# write.csv(missing_table, file = "f_w_missing.csv", row.names = TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- R_Scripts/MatthewR/Category Analysis/World Stats/world_stats_analysis.R
+++ R_Scripts/MatthewR/Category Analysis/World Stats/world_stats_analysis.R
@@ -11,6 +11,7 @@ library(visdat)
 library("mice")
 library(UpSetR)
 library("janitor")
+library(corrplot)
 
 # We read in the data and get it into a good form to use MICE imputation by cleaning the titles
 # and removing the column the CSV file has added.
@@ -19,12 +20,15 @@ clean_world_stats <- read_csv("Combined DataFrame Work/CSV Files/Clean/clean_wor
 clean_world_stats <- clean_names(clean_world_stats)
 clean_world_stats <- subset(clean_world_stats, select = -1)
 
-# We look at the missing data
+summary(clean_world_stats)
 
-missing_data_plot <- vis_miss(clean_world_stats, sort_miss = TRUE)
+# We look at the missing data
+clean_world_stats_new_name <- clean_world_stats
+colnames(clean_world_stats_new_name) <- c("Country", "Total Confirmed Deaths per Million", "Life Satisfaction Indicator")
+missing_data_plot <- vis_miss(clean_world_stats_new_name, sort_miss = TRUE)
 miss_var_summary(clean_world_stats)
 
-# ggsave(missing_data_plot, file="w_s_missing_data.png")
+ggsave(missing_data_plot, file="w_s_missing_data.png")
 
 
 # Summary Statistics - this removes the missing values.
@@ -32,9 +36,9 @@ miss_var_summary(clean_world_stats)
 summary_w_missing <- ggplot(data = clean_world_stats, aes(x = life_satisfaction_in_cantril_ladder_world_happiness_report_2019, y = total_confirmed_deaths_due_to_covid_19_per_million_people)) + 
   geom_point() +
   ggtitle("Life Satisfaction Indicator and Covid Death Rates") + labs(x="Life Satisfaction Indicator", y = "Total Confirmed Deaths due to Covid-19 per Million People") + 
-  geom_smooth(method = "lm", se = FALSE) + theme(text = element_text(size=10))
+  geom_smooth(method = "lm", se = FALSE) + theme(text = element_text(size=12))
 
-#ggsave(summary_w_missing, file="w_s_summary_stat_w_missing.png")
+ggsave(summary_w_missing, file="w_s_summary_stat_w_missing.png")
 
 # We now do the MICE imputation.
 # We use set seed to give us the same result when we run our code again.
@@ -53,6 +57,7 @@ world_stats_model <- with(world_stats_imputation, lm(total_confirmed_deaths_due_
 
 summary(world_stats_model)
 summary(pool(world_stats_model))
+
 
 # Now do summary statistics for the imputed data.
 
@@ -78,30 +83,24 @@ ws_mice_5 <- ggplot(data = world_stats_5, aes(x = life_satisfaction_in_cantril_l
 
 ws_iumputed_summary <- grid.arrange(ws_mice_1, ws_mice_2, ws_mice_3, ws_mice_4, ws_mice_5, nrow = 3,
                                     left = "Total Confirmed Deaths due to Covid-19 per Million People",
-                                    top = textGrob("Life Satisfaction and Covid-19 Death Rates (Imputed)", gp=gpar(col="red", fontface = "bold", fontsize = 12)),
+                                    top = textGrob("Life Satisfaction and Covid-19 Death Rates (Imputed)", gp=gpar(col="red", fontface = "bold", fontsize = 15)),
                                     bottom = "Life Satisfaction Indicator")
 
 
 ggsave(ws_iumputed_summary, file="w_s_imputed_sum.png")
   
 
+numeric_dat <- world_stats_1[,2:3]
+M <- cor(numeric_dat)
+colnames(M) <- c("Deaths", "Life Sat")
+rownames(M) <- c("Deaths", "Life Sat")
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#png(height = 6.83, width = 6.08, units = "in",res = 1000, file = "w_s_correlation_plot.png")
+corrplot(M, method = 'square', order = 'AOE', addCoef.col = 'black', tl.pos = 'd',
+         cl.pos = 'n', col = COL2('BrBG'), title = "Correlation Plot", mar=c(0,0,1,0))
+#dev.off()
 
 
 
