cafd51803b4b403d9b8044efc44ade34b207e881
MattthewD <66824293+MattthewD@users.noreply.github.com>
2022-01-25 13:16:33 +0000

    MD Committ

--- R_Scripts/MatthewR/Category Analysis/3. RF/RF attempt.R
+++ R_Scripts/MatthewR/Category Analysis/3. RF/RF attempt.R
@@ -1,11 +1,33 @@
 # Trying random forests
-
+library("ggplot2")
+library(grid)
+library(gridExtra)
+library(lattice)
+library("tidyverse")
+library("readr")
+library(dplyr)
+library(naniar)
+library(car)
+library(visdat)
+library("mice")
+library(UpSetR)
+library("janitor")
+library(corrplot)
 library('missForest')
 
-Name <- c("A", "B", "C", "D", "E", "F")
-Age <- c(23, 41, 32, 58, 26, 27)
-Height <- c(150,120,140, NA,150, 100)
 
+# Learning how RF works.
+Name <- c(1:100)
+Age <- c(3:102)
+Height <- c(1:99,NA)
 df <- data.frame(Name, Age, Height)
-
 missForest(df,ntree = 100)
+
+
+
+
+
+
+
+
+
