460c975e58503801af03a8c621f3e59923bea282
esaheed <s1858135@ed.ac.uk>
2022-01-28 15:04:14 +0000

    random forest and correlation

---
+++ R_Scripts/EmmaR/healthcare_analysis_withRF.R
@@ -0,0 +1,120 @@
+library(readr)
+library(mice)
+library(VIM)
+library(janitor)
+library(car)
+library(MuMIn)
+library(readr)
+library(ggplot2)
+library(naniar)
+library(visdat)
+library(missForest)
+
+# Import data 
+clean_healthcare <- read_csv("Combined DataFrame Work/CSV Files/Clean/clean_healthcare.csv")
+clean_healthcare <- subset(clean_healthcare, select = -1)
+clean_healthcare <- clean_names(clean_healthcare)
+
+# View missing data
+summary(clean_healthcare)
+
+missing_table <- miss_var_summary(clean_healthcare, sort_miss = TRUE)
+missing_table <- data.frame(missing_table)
+missing_table
+
+# View variables with more than 50% of missing data
+for (i in 1:78){
+  if (missing_table$pct_miss[i] > 50){
+    print(missing_table$variable[i]) 
+  } 
+}
+
+
+# Dropping data below
+# Following have more than 50% of missing data (and another reason for dropping data) 
+
+drop <- c("percentage_of_persons_without_health_insurance_percent", #including share_of_population_covered_by_health_insurance_ilo_2014
+          "standard_deviation_of_life_satisfaction", # more than 50% missing data and seems irrelevant
+          "public_expenditure_on_health_tanzi_schuktnecht_2000", # including health_expenditure_per_capita_ppp_world_bank_2016
+          "hospital_beds_nurse_to_bed_ratio_oecd", # including beds and nurses
+          "beds_in_not_for_profit_privately_owned_hospitals_number_oecd",# including beds_in_not_for_profit_privately_owned_hospitals_per_1_000_population_oecd
+          "nurses_headcount_oecd", # including nurses_per_1_000_population_oecd
+          "dentists_headcount_oecd", # including dentists_per_100_000_population_oecd
+          "midwives_headcount_oecd", # including midwives_per_1_000_live_births_oecd
+          "not_for_profit_privately_owned_hospitals_number_oecd", # including not_for_profit_privately_owned_hospitals_per_million_population_oecd
+          "beds_in_for_profit_privately_owned_hospitals_number_oecd", # including beds_in_for_profit_privately_owned_hospitals_per_1_000_population_oecd
+          "for_profit_privately_owned_hospitals_number_oecd", # including for_profit_privately_owned_hospitals_per_million_population_oecd
+          "physicians_headcount_oecd", # including physicians_per_1_000_population_oecd
+          "publicly_owned_hospitals_number_oecd", # including publicly_owned_hospitals_per_million_population_oecd
+          "beds_in_publicly_owned_hospitals_number_oecd", # including beds_in_publicly_owned_hospitals_per_1_000_population_oecd
+          "long_term_care_beds_number_oecd", # including long_term_care_beds_per_1_000_population_oecd and long_term_care_beds_per_1_000_population_aged_65_oecd
+          "general_hospitals_number_oecd", # including general_hospitals_per_million_population_oecd
+          "hospitals_number_oecd", # including hospitals_per_million_population_oecd
+          "health_expenditure_and_financing_per_capita_oec_dstat_2017",
+          "surgical_specialists_headcount_oecd", # including surgical_specialists_per_1_000_population_oecd
+          "health",
+          "acute_care_beds_number_oecd", # including acute_care_beds_per_1_000_population_oecd
+          "psychiatrists_headcount_oecd", # including psychiatrists_per_1_000_population_oecd
+          "psychiatric_care_beds_number_oecd", # including psychiatric_care_beds_per_1_000_population_oecd
+          "old_age",
+          "how_much_we_think_we_spend_on_health_expenditure_ipsos_2016",
+          "how_much_we_actually_spend_on_health_expenditure_ipsos_2016",
+          "hospital_beds_number_oecd") # including hospital_beds_per_1_000_population_oecd
+
+
+### The following data has over 50% of missing data but has been decided to keep ###
+# beds_in_not_for_profit_privately_owned_hospitals_per_1_000_population_oecd
+# nurses_per_1_000_population_oecd
+# dentists_per_100_000_population_oecd
+# midwives_per_1_000_live_births_oecd
+# not_for_profit_privately_owned_hospitals_per_million_population_oecd
+# beds_in_for_profit_privately_owned_hospitals_per_1_000_population_oecd
+# for_profit_privately_owned_hospitals_per_million_population_oecd
+# physicians_per_1_000_population_oecd
+# publicly_owned_hospitals_per_million_population_oecd
+# beds_in_publicly_owned_hospitals_per_1_000_population_oecd
+# long_term_care_beds_per_1_000_population_oecd
+# long_term_care_beds_per_1_000_population_aged_65_oecd
+# general_hospitals_per_million_population_oecd
+# hospitals_per_million_population_oecd
+# surgical_specialists_per_1_000_population_oecd
+# acute_care_beds_per_1_000_population_oecd
+# psychiatrists_per_1_000_population_oecd
+# psychiatric_care_beds_per_1_000_population_oecd
+# hospital_beds_per_1_000_population_oecd
+
+
+# Create 'healthcare_d1' without variables with > 50% missing data + justified reason for removing
+healthcare_rf <- clean_healthcare[,!(names(clean_healthcare) %in% drop)]
+healthcare_rf <- subset(healthcare_rf, select = -1)
+
+healthcare_rf <- as.matrix(healthcare_rf)
+healthcare_rf_temp <- missForest(healthcare_rf, maxiter = 5)
+
+healthcare_rf_completed <- healthcare_rf_temp$ximp
+
+healthcare_rf_completed <- data.frame(healthcare_rf_completed)
+
+# Compute correlation 
+
+corr_data <- healthcare_rf_completed[,2:50]  
+
+healthcare_corr <- vis_cor(corr_data) + theme(axis.text.x = element_text(angle = 90)) + 
+  ggtitle("Correlation Matrix")
+
+healthcare_corr_df <- round(cor(corr_data),2)
+
+View(healthcare_corr_df)
+
+write.csv(healthcare_corr_df,"healthcare_corr.csv", row.names = TRUE)
+
+healthcare_corr <- read_csv("R_Scripts/EmmaR/healthcare_corr.csv")
+View(healthcare_corr)
+
+# Remove any variables with corr = 1
+#[32] "healthy_life_expectancy_ihme"                                                            
+#[33] "life_expectancy_ihme" 
+
+healthcare_rf_completed <- subset(healthcare_rf_completed, select = -c(32, 33))
+
+# VIF 
