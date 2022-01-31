3276e66500defbeb96213410a6ed972bb9e3e2ef
panzershreks <weman@gmail.com>
2022-01-31 13:21:08 +0000

    Added functionality for only numerical vars

--- R_Scripts/EmanR/automate_vif.R
+++ R_Scripts/EmanR/automate_vif.R
@@ -7,17 +7,27 @@ library(car)
 #' @param vif_max num for max VIF allowed (not GVIF)
 #' @return list of str of explanatory variables after dropping
 gvif_drop <- function(resp_var, expl_var, data, vif_max=10) {
-    gvif_max <- vif_max ^ 0.5
+  gvif_max <- vif_max ^ 0.5
+  lm_formula <- lm_formula_paster(resp_var, expl_var)
+  model <- lm(lm_formula, data)
+  vif_mod <- vif(model)
+  try(gvif <- vif_mod, silent = TRUE)
+  try(gvif <- vif_mod[,3], silent = TRUE)
+  if (is.null(dim(vif_mod))) {
+    gvif_max <- vif_max
+  }
+  while (max(gvif) > gvif_max) {
+    expl_var <- expl_var[-(which.max(gvif))]
     lm_formula <- lm_formula_paster(resp_var, expl_var)
     model <- lm(lm_formula, data)
     vif_mod <- vif(model)
-    while (max(vif_mod[,3]) > gvif_max) {
-        expl_var <- expl_var[-(which.max(vif_mod[,3]))]
-        lm_formula <- lm_formula_paster(resp_var, expl_var)
-        model <- lm(lm_formula, data)
-        vif_mod <- vif(model)
+    try(gvif <- vif_mod, silent = TRUE)
+    try(gvif <- vif_mod[,3], silent = TRUE)
+    if (is.null(dim(vif_mod))) {
+      gvif_max <- vif_max
     }
-    return (expl_var)
+  }
+  return (expl_var)
 }
 
 #' create a lm formula from list of variables
@@ -26,12 +36,12 @@ gvif_drop <- function(resp_var, expl_var, data, vif_max=10) {
 #' @param expl_var list of str of explanatory variables
 #' @return str of formula using the variables provided
 lm_formula_paster <- function(resp_var, expl_var) {
-    form <- paste0(resp_var, "~")
-    for (var in head(expl_var, -1)) {
-        form <- paste0(form, var, "+")
-    }
-    form <- paste0(form, tail(expl, 1))
-    return (form)
+  form <- paste0(resp_var, "~")
+  for (var in head(expl_var, -1)) {
+    form <- paste0(form, var, "+")
+  }
+  form <- paste0(form, tail(expl_var, 1))
+  return (form)
 }
 
 data(iris)
@@ -44,8 +54,7 @@ final_formula <- lm_formula_paster(resp, after_drop)
 final_model <- lm(final_formula, iris)
 vif(final_model)
 
-
-# If we did this manually:
+# If we did this manually (when categorical variable is present)
 model1 <- lm(Sepal.Length~Sepal.Width+Petal.Length+Petal.Width+Species, iris)
 vif(model1) 
 # remove Petal.Length
@@ -56,12 +65,20 @@ model3 <- lm(Sepal.Length~Sepal.Width+Species, iris)
 vif(model3) # final model
 
 
+resp2 <- "Sepal.Length"
+expl2 <- c("Sepal.Width", "Petal.Length", "Petal.Width")
 
+after_drop2 <- gvif_drop(resp2, expl2, iris)
+final_formula2 <- lm_formula_paster(resp2, after_drop2)
+final_model2 <- lm(final_formula2, iris)
+vif(final_model2)
 
-
-
-
-
+# (when there are no categorical variables)
+modelA <- lm(Sepal.Length~Sepal.Width+Petal.Length+Petal.Width, iris)
+vif(modelA)
+# remove Petal.Length
+modelB <- lm(Sepal.Length~Sepal.Width+Petal.Width, iris)
+vif(modelB) # final model
 
 
 
