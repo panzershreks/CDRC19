a5a295145710adc40115b0d4179e1a92ad878f5e
MattthewD <66824293+MattthewD@users.noreply.github.com>
2022-01-31 15:51:54 +0000

    MD

---
+++ R_Scripts/MatthewR/New Categories Analysis/disease.R
@@ -0,0 +1,78 @@
+library("ggplot2")
+library(grid)
+library(gridExtra)
+library(lattice)
+library("tidyverse")
+library("readr")
+library(dplyr)
+library(naniar)
+library(car)
+library(visdat)
+library("mice")
+library(UpSetR)
+library("janitor")
+library(corrplot)
+library('missForest')
+library(readr)
+
+clean_disease <- read_csv("Combined DataFrame Work/CSV Files/Clean/clean_disease.csv")
+clean_disease <- clean_names(clean_disease)
+clean_disease <- subset(clean_disease, select = -c(1,2))
+
+
+# We now remove the countries which have no response variable value.
+
+clean_disease <- clean_disease[-c(20,29,48,54,56,67,88,91,106,112,118,125,126,130,142,143,144,
+                                        145, 151, 156, 171,173,177,178, 186,193),]
+
+set.seed(100)
+disease_rf <- missForest(as.matrix(clean_disease))
+disease_data <- disease_rf$ximp
+disease_data <- as.data.frame.matrix(disease_data)
+
+
+resp <- "total_confirmed_deaths_due_to_covid_19_per_million_people"
+expl <- c("age_standardised_diabetes_prevalence_male","age_standardised_diabetes_prevalence_female",
+          "crude_diabetes_prevalence_male",
+          "crude_diabetes_prevalence_female",
+          "ratio_of_diabetes_to_obesity_prevalence",
+          "ratio_of_diabetes_to_overweight_prevalence",
+          "deaths_no_access_to_handwashing_facility_sex_both_age_70_number",
+          "deaths_unsafe_sanitation_sex_both_age_70_number",
+          "deaths_unsafe_water_source_sex_both_age_70_number",
+          "deaths_no_access_to_handwashing_facility_sex_both_age_70_r_ate",
+          "deaths_unsafe_sanitation_sex_both_age_70_rate",
+          "deaths_unsafe_water_source_sex_both_age_70_rate",
+          "male_lung_cancer_deaths_per_100_000_who_iarc_2016",
+          "dementia_ihme_2017","cardiovascular_diseases_ihme_2017",
+          "stroke_ihme_2017","kidney_disease_ihme_2017","respiratory_disease_ihme_2017",
+          "liver_disease_ihme_2017","diabetes_blood_and_endocrine_disease_ihme_2017",
+          "digestive_disease_ihme_2017","hepatitis_ihme_2017","meningitis_ihme_2017",
+          "cancers_ihme_2017","parkinsons_disease_ihme_2017",
+          "prevalence_of_obesity_both_sexes_who_2019",
+          "prevalence_of_obesity_male_who_2019","prevalence_of_obesity_female_who_2019")
+
+after_drop <- gvif_drop(resp, expl, disease_data)
+final_formula <- lm_formula_paster(resp, after_drop)
+final_model <- lm(final_formula, disease_data)
+vif(final_model)
+
+step_disease <- step(final_model)
+summary(step_disease)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ R_Scripts/MatthewR/New Categories Analysis/food_water.R
@@ -0,0 +1,87 @@
+library("ggplot2")
+library(grid)
+library(gridExtra)
+library(lattice)
+library("tidyverse")
+library("readr")
+library(dplyr)
+library(naniar)
+library(car)
+library(visdat)
+library("mice")
+library(UpSetR)
+library("janitor")
+library(corrplot)
+library('missForest')
+library(readr)
+
+# We read in the data we require, and get it into the format we want.
+
+clean_food_water <- read_csv("Combined DataFrame Work/CSV Files/Clean/clean_food_water.csv")
+clean_food_water <- clean_names(clean_food_water)
+clean_food_water <- subset(clean_food_water, select = -c(1,2))
+
+# We now remove the countries which have no response variable value.
+
+clean_food_water <- clean_food_water[-c(20,29,48,54,56,67,88,91,106,112,118,125,126,130,142,143,144,
+                                        145, 151, 156, 171,173,177,178, 186,193), ]
+
+# We now do the Random Forest Data imputation
+
+set.seed(100)
+food_rf <- missForest(as.matrix(clean_food_water))
+
+food_data <- food_rf$ximp
+food_data <- as.data.frame.matrix(food_data)
+
+
+# We now will use a function we have written to carry out VIF to 
+# find what variables we should use in our model.
+
+resp <- "total_confirmed_deaths_due_to_covid_19_per_million_people"
+expl <- c("cost_of_calorie_sufficient_diet_2017_usd_per_day",
+           "cost_of_nutrient_adequate_diet_2017_usd_per_day",
+           "cost_of_healthy_diet_2017_usd_per_day",
+           "calorie_sufficient_diet_cost_percent_of_1_20_poverty_line",
+           "nutrient_adequate_diet_cost_percent_of_1_20_poverty_line",
+           "healthy_diet_cost_percent_of_1_20_poverty_line",
+           "calorie_sufficient_diet_cost_percent_of_average_food_expenditure",
+           "nutrient_adequate_diet_cost_percent_of_average_food_expenditure",
+           "healthy_diet_cost_percent_of_average_food_expenditure",
+           "calorie_sufficient_diet_cost_percent_cannot_afford",
+           "nutrient_adequate_diet_cost_percent_cannot_afford",
+           "healthy_diet_cost_percent_cannot_afford",
+           "calorie_sufficient_diet_cost_number_cannot_afford",
+           "nutrient_adequate_diet_cost_number_cannot_afford",
+           "healthy_diet_cost_number_cannot_afford",
+           "population_with_access_to_improved_sanitation_y",
+           "population_without_access_to_improved_sanitation_y")
+
+after_drop <- gvif_drop(resp, expl, food_data)
+final_formula <- lm_formula_paster(resp, after_drop)
+final_model <- lm(final_formula, food_data)
+vif(final_model)
+
+step_food <- step(final_model)
+summary(step_food)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
