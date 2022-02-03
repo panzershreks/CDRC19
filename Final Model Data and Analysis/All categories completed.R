3db4eab88a01a4628779aedd8ee06ffa23c5af76
esaheed <s1858135@ed.ac.uk>
2022-02-03 12:59:09 +0000

    adding demographic data

--- Final Model Data and Analysis/All categories completed.R
+++ Final Model Data and Analysis/All categories completed.R
@@ -27,11 +27,9 @@ rf_enviroment_imputed_data_variables <- subset(rf_enviroment_imputed_data_variab
 econ_significant <- read_csv("Final Model Data and Analysis/Categories Complete/econ_significant.csv")
 econ_significant <- subset(econ_significant, select = -1)
 
-# NEED TO CHANGE THIS POTENTIALLY 
 Completed_data_demorgraphic_ <- read_csv("Final Model Data and Analysis/Categories Complete/Completed data(demorgraphic).csv")
 Completed_data_demorgraphic_ <- subset(Completed_data_demorgraphic_, select = -1)
 
-# NEED TO ADD DEMOGRAPHIC TO THIS 
 all_categories_complete <- cbind(matthew_sig_var_complete, healthcare_sigvars_completed, rf_covid_imputed_data_variables, 
                                  rf_enviroment_imputed_data_variables, econ_significant,Completed_data_demorgraphic_)
   
@@ -83,27 +81,44 @@ lm_formula_paster <- function(resp_var, expl_var) {
 resp <- "total_confirmed_deaths_due_to_covid_19_per_million_people"
 
 # NEED TO ADD THIS 
-expl <- c("age_standardised_diabetes_prevalence_male","cardiovascular_diseases_ihme_2017",
-          "meningitis_ihme_2017","prevalence_of_obesity_female_who_2019",
+expl <- c("age_standardised_diabetes_prevalence_male",
+          "cardiovascular_diseases_ihme_2017",
+          "meningitis_ihme_2017",
+          "prevalence_of_obesity_female_who_2019",
           "healthy_diet_cost_percent_cannot_afford",
           "share_of_people_who_disagree_vaccines_are_important_for_children_to_have",
-          "share_of_people_who_disagree_vaccines_are_safe","share_of_people_who_agree_vaccines_are_effective",
-          "general_hospitals_per_million_population_oecd","not_for_profit_privately_owned_hospitals_per_million_population_oecd",
+          "share_of_people_who_disagree_vaccines_are_safe",
+          "share_of_people_who_agree_vaccines_are_effective",
+          "general_hospitals_per_million_population_oecd",
+          "not_for_profit_privately_owned_hospitals_per_million_population_oecd",
           "psychiatric_care_beds_per_1_000_population_oecd",
           "publicly_owned_hospitals_per_million_population_oecd",
           "out_of_pocket_expenditure_per_capita_on_healthcare_ppp_usd_who_global_health_expenditure",
-          "case_fatality_rate_of_covid_19_short_term","biweekly_deaths_per_million_people",
-          "weekly_cases_per_million_people","weekly_case_growth","doubling_days_of_total_confirmed_deaths_3_day_period",
-          "days_since_10_daily_new_confirmed_deaths_recorded","case_fatality_rate_of_covid_19_only_observations_with_100_cases",
-          "days_since_30_daily_new_confirmed_cases_recorded","total_confirmed_deaths_due_to_covid_19",
+          "case_fatality_rate_of_covid_19_short_term",
+          "biweekly_deaths_per_million_people",
+          "weekly_cases_per_million_people",
+          "weekly_case_growth",
+          "doubling_days_of_total_confirmed_deaths_3_day_period",
+          "days_since_10_daily_new_confirmed_deaths_recorded",
+          "case_fatality_rate_of_covid_19_only_observations_with_100_cases",
+          "days_since_30_daily_new_confirmed_cases_recorded",
+        # "total_confirmed_deaths_due_to_covid_19",
           "daily_new_confirmed_cases_of_covid_19_per_million_people",
           "daily_new_confirmed_deaths_due_to_covid_19_per_million_people",
-          "total_confirmed_cases_of_covid_19_per_million_people","population_with_access_to_improved_sanitation_x",
-          "debt_relief","stringency_index","deaths_from_anthropogenic_pollution_as_a_share_of_total_air_pollution_deaths",
-          "yll_rates_from_anthropogenic_air_pollution_per_100_000","income_classification_world_bank_2017",
-          "gdp_growth_from_previous_year_2020_q2","gdp","national_poverty_lines_jolliffe_and_prydz_2016",
+          "total_confirmed_cases_of_covid_19_per_million_people",
+          "population_with_access_to_improved_sanitation_x",
+          "debt_relief",
+          "stringency_index",
+          "yll_rates_from_anthropogenic_air_pollution_per_100_000",
+          "deaths_from_fossil_pollution_as_a_share_of_total_anthropogenic_air_pollution_deaths", 
+          "deaths_from_anthropogenic_pollution_as_a_share_of_total_air_pollution_deaths",
+          "income_classification_world_bank_2017",
+          "gdp_growth_from_previous_year_2020_q2",
+          "gdp",
+          "national_poverty_lines_jolliffe_and_prydz_2016",
           "percentage_contribution_of_deprivations_in_education_to_overall_poverty_alkire_and_robles_2016",
-          "multidimensional_poverty_headcount_ratio_alkire_and_robles_2016","Infant.mortality.rate")
+          "multidimensional_poverty_headcount_ratio_alkire_and_robles_2016",
+          "Infant.mortality.rate")
 
 after_drop <- gvif_drop(resp, expl, all_categories_complete)
 final_formula <- lm_formula_paster(resp, after_drop)
@@ -118,5 +133,7 @@ summary(step_all_categories)
 par(mfrow = c(2, 2))
 plot(step_all_categories)
 
+
+colnames(all_categories_complete)
 
 
