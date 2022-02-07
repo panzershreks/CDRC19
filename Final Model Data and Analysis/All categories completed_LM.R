d4624d5a3e0e1fc871df532f8a11b0b18ba235af
esaheed <s1858135@ed.ac.uk>
2022-02-07 10:32:13 +0000

    rename folder

--- Final Model Data and Analysis/All categories completed_LM.R
+++ Final Model Data and Analysis/All categories completed_LM.R
@@ -35,6 +35,7 @@ all_categories_complete <- cbind(matthew_sig_var_complete, healthcare_sigvars_co
 
 View(all_categories_complete)
 
+colnames(all_categories_complete)
 # Automated VIF function 
 
 # Using score threshold of 5 
@@ -79,40 +80,32 @@ lm_formula_paster <- function(resp_var, expl_var) {
 
 resp <- "total_confirmed_deaths_due_to_covid_19_per_million_people"
 
-expl <- c("age_standardised_diabetes_prevalence_male",
-          "cardiovascular_diseases_ihme_2017",
-          "meningitis_ihme_2017",
-          "prevalence_of_obesity_female_who_2019",
-          "healthy_diet_cost_percent_cannot_afford",
-          "share_of_people_who_disagree_vaccines_are_important_for_children_to_have",
-          "share_of_people_who_disagree_vaccines_are_safe",
-          "share_of_people_who_agree_vaccines_are_effective",
-          "general_hospitals_per_million_population_oecd",
-          "not_for_profit_privately_owned_hospitals_per_million_population_oecd",
-          "psychiatric_care_beds_per_1_000_population_oecd",
-          "publicly_owned_hospitals_per_million_population_oecd",
-          "out_of_pocket_expenditure_per_capita_on_healthcare_ppp_usd_who_global_health_expenditure",
-          "case_fatality_rate_of_covid_19_short_term",
-          "biweekly_deaths_per_million_people",
-          "weekly_cases_per_million_people",
-          "weekly_case_growth",
-          "doubling_days_of_total_confirmed_deaths_3_day_period",
-          "days_since_10_daily_new_confirmed_deaths_recorded",
-          "case_fatality_rate_of_covid_19_only_observations_with_100_cases",
-          "days_since_30_daily_new_confirmed_cases_recorded",
-          "daily_new_confirmed_cases_of_covid_19_per_million_people",
-          "total_confirmed_cases_of_covid_19_per_million_people",
-          "population_with_access_to_improved_sanitation_x",
-          "debt_relief",
-          "stringency_index",
-          "deaths_from_anthropogenic_pollution_as_a_share_of_total_air_pollution_deaths",
-          "yll_rates_from_anthropogenic_air_pollution_per_100_000",
-          "income_classification_world_bank_2017",
-          "gdp_growth_from_previous_year_2020_q2",
-          "gdp",
-          "national_poverty_lines_jolliffe_and_prydz_2016",
+expl <- c("total_confirmed_deaths_due_to_covid_19_per_million_people",                                     
+          "healthy_diet_cost_percent_cannot_afford",                                                    
+          "cost_of_calorie_sufficient_diet_2017_usd_per_day",                                              
+          "healthy_diet_cost_percent_of_1_20_poverty_line",                                                
+          "life_satisfaction_in_cantril_ladder_world_happiness_report_2019",                               
+          "age_standardised_diabetes_prevalence_male",                                                     
+          "cardiovascular_diseases_ihme_2017",                                                             
+          "meningitis_ihme_2017",                                                                          
+          "prevalence_of_obesity_female_who_2019",                                                         
+          "kidney_disease_ihme_2017",                                                                      
+          "diabetes_blood_and_endocrine_disease_ihme_2017",                                                
+          "all_causes_disability_adjusted_life_years_who_2015",                                            
+          "beds_in_not_for_profit_privately_owned_hospitals_per_1_000_population_oecd",                    
+          "long_term_care_beds_per_1_000_population_oecd",                                                 
+          "publicly_owned_hospitals_per_million_population_oecd",                                          
+          "surgical_specialists_per_1_000_population_oecd",                                                
+          "income_support",                                                                                
+          "containment_index",                                                                             
+          "deaths_from_anthropogenic_pollution_as_a_share_of_total_air_pollution_deaths",                  
+          "yll_rates_from_anthropogenic_air_pollution_per_100_000",                                        
+          "income_classification_world_bank_2017",                                                         
+          "gdp_growth_from_previous_year_2020_q2",                                                         
+          "gdp",                                                                                           
+          "national_poverty_lines_jolliffe_and_prydz_2016",                                                
           "percentage_contribution_of_deprivations_in_education_to_overall_poverty_alkire_and_robles_2016",
-          "multidimensional_poverty_headcount_ratio_alkire_and_robles_2016",
+          "multidimensional_poverty_headcount_ratio_alkire_and_robles_2016",                               
           "Infant.mortality.rate")
       
 
