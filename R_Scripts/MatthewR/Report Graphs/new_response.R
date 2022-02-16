af8a281651aea370b893032d9dcacefc2f4a266f
MattthewD <66824293+MattthewD@users.noreply.github.com>
2022-02-16 11:33:46 +0000

    MD

--- R_Scripts/MatthewR/Report Graphs/new_response.R
+++ R_Scripts/MatthewR/Report Graphs/new_response.R
@@ -16,6 +16,7 @@ library("janitor")
 library(corrplot)
 library('missForest')
 library(readr)
+library(scales)
 
 
 clean_world_stats <- read_csv("Combined DataFrame Work/CSV Files/Clean/clean_world_stats.csv")
@@ -30,41 +31,28 @@ clean_world_stats$total_confirmed_deaths_due_to_covid_19_per_million_people
 
 cp_1 <- ggplot(data = clean_world_stats[0:50,], aes(x = entity , y = total_confirmed_deaths_due_to_covid_19_per_million_people)) + 
   geom_point()  + xlab(NULL) +
-  ylab(NULL) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+  ylab(NULL) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +  scale_x_discrete(label=function(x) abbreviate(x, minlength=7))
 
 cp_2 <- ggplot(data = clean_world_stats[51:100,], aes(x = entity , y = total_confirmed_deaths_due_to_covid_19_per_million_people)) + 
   geom_point() + xlab(NULL) +
-  ylab(NULL)  + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+  ylab(NULL)  + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +  scale_x_discrete(label=function(x) abbreviate(x, minlength=7))
 
 cp_3 <- ggplot(data = clean_world_stats[101:150,], aes(x = entity , y = total_confirmed_deaths_due_to_covid_19_per_million_people)) + 
   geom_point() + xlab(NULL) +
-  ylab(NULL) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+  ylab(NULL) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +  scale_x_discrete(label=function(x) abbreviate(x, minlength=7))
 
 cp_4 <- ggplot(data = clean_world_stats[151:193,], aes(x = entity , y = total_confirmed_deaths_due_to_covid_19_per_million_people)) + 
   geom_point() + xlab(NULL) +
-  ylab(NULL)  + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+  ylab(NULL)  + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +  scale_x_discrete(label=function(x) abbreviate(x, minlength=7))
+
+
+
 
 covid_summary_plot <- grid.arrange(cp_1, cp_2, cp_3, cp_4, left = "Total Confirmed Deaths due to Covid-19 per Million People", 
-                                   top = textGrob("Covid-19 Death Data", gp=gpar(col="red", fontface = "bold", fontsize = 15)))
+                                   top = textGrob("Covid-19 Death Data", gp=gpar(col="black", fontface = "bold", fontsize = 15)))
+ 
 
-#ggsave(covid_summary_plot, filename = "death_summary.png", height = 8, width = 14, units = "in")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#ggsave(covid_summary_plot, filename = "R_Scripts//MatthewR//Report Graphs//death_summary.png", height = 8, width = 14, units = "in")
 
 
 
