bcba013d0f472a17bca65153f561250cc3a85dcc
MattthewD <66824293+MattthewD@users.noreply.github.com>
2022-02-21 22:48:50 +0000

    MD

--- R_Scripts/MatthewR/Report Graphs/Time Series.R
+++ R_Scripts/MatthewR/Report Graphs/Time Series.R
@@ -28,7 +28,7 @@ smooth_new_death_plot <- ggplot(data = world_data, aes(Date, Smooth_New_Deaths))
 
 death_world <- grid.arrange(cum_death_plot, smooth_new_death_plot, top = textGrob("Worldwide Covid-19 Deaths", gp=gpar(col="black", fontface = "bold", fontsize = 15)))
 
-ggsave(death_world, filename = "R_Scripts//MatthewR//Report Graphs//deaths_world.png", height = 8, width = 14, units = "in")
+#ggsave(death_world, filename = "R_Scripts//MatthewR//Report Graphs//deaths_world.png", height = 8, width = 14, units = "in")
 
 
 # We now want to do the same for the UK
@@ -50,6 +50,25 @@ death_uk <- grid.arrange(cum_death_plot, smooth_new_death_plot, top = textGrob("
 
 #ggsave(death_uk, filename = "R_Scripts//MatthewR//Report Graphs//deaths_uk.png", height = 8, width = 14, units = "in")
 
-smooth_new_death_plot
 
+
+nz_data <- subset_df %>% filter(owid_covid_data.location == 'New Zealand')
+nz_data <- subset(nz_data, select = -c(owid_covid_data.location))
+colnames(nz_data) <- c("Date", "Cumulative_Deaths", "New_Deaths", "Smooth_New_Deaths")
+
+aus_data <- subset_df %>% filter(owid_covid_data.location == 'Australia')
+aus_data <- subset(aus_data, select = -c(owid_covid_data.location))
+colnames(aus_data) <- c("Date", "Cumulative_Deaths", "New_Deaths", "Smooth_New_Deaths")
+
+aus_plot <- ggplot(data = aus_data, aes(Date, Smooth_New_Deaths)) + geom_bar(stat = "identity", fill = "aquamarine3") +
+  labs(title = "Australia Covid-19 Deaths",
+       x = "Date", y = "New Daily Covid-19 Deaths (Smoothed)") + scale_y_continuous(labels = comma) + scale_x_date(date_breaks = "1 month", date_labels =  "%m-%y")
+
+nz_plot <- ggplot(data = nz_data, aes(Date, Smooth_New_Deaths)) + geom_bar(stat = "identity", fill = "aquamarine3") +
+  labs(title = "New Zealand Covid-19 Deaths",
+       x = "Date", y = "New Daily Covid-19 Deaths (Smoothed)") + scale_y_continuous(labels = comma) + scale_x_date(date_breaks = "1 month", date_labels =  "%m-%y")
+
+death_nz_aus <- grid.arrange(aus_plot, nz_plot, top = textGrob("Covid-19 Deaths Comparison", gp=gpar(col="black", fontface = "bold", fontsize = 15)))
+
+#ggsave(death_nz_aus, filename = "R_Scripts//MatthewR//Report Graphs//deaths_nz_aus.png", height = 8, width = 14, units = "in")
 
