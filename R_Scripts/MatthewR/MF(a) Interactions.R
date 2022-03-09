3edfd14485f48c5cd37a9b67c6cacb47d13b9a3e
panzershreks <weman@gmail.com>
2022-03-09 17:06:41 +0000

    contour plot code

--- R_Scripts/MatthewR/MF(a) Interactions.R
+++ R_Scripts/MatthewR/MF(a) Interactions.R
@@ -1,5 +1,7 @@
 # MF(a) interactions
 library(interactions)
+library(MuMIn)
+library(ggplot2)
 source("GLM Take 2/Combined Model/Full Model Imputation.R")
 
 fit_a <- glm(total_confirmed_deaths_due_to_covid_19_per_million_people ~ 
@@ -16,12 +18,26 @@ fit_b <- glm(total_confirmed_deaths_due_to_covid_19_per_million_people ~
               prevalence_of_obesity_both_sexes_who_2019:age_standardised_diabetes_prevalence_male, 
              data = combined_imputed_res, family = Gamma(link = "log"))
 
-AIC(fit_a)
-AIC(fit_b)
+AICc(fit_a)
+AICc(fit_b)
 
 interact_plot(fit_b, pred = prevalence_of_obesity_both_sexes_who_2019, 
               modx = age_standardised_diabetes_prevalence_male, plot.points = TRUE, 
               main.title = "Interaction Between Obesity and Diabetes")
+
+
+grid.lines=100
+X3 <- combined_imputed_res$prevalence_of_obesity_both_sexes_who_2019
+X5 <- combined_imputed_res$age_standardised_diabetes_prevalence_male
+b3 <- 0.2440961
+b5 <- 0.2599402
+b6 <- -0.0144685
+
+X3grid <- seq(min(X3), max(X3), length.out = grid.lines)
+X5grid <- seq(min(X5), max(X5), length.out = grid.lines)
+data.fit <- expand.grid(X3 = X3grid, X5 = X5grid)
+data.fit['inter'] <- b3 * data.fit['X3'] + b5 * data.fit['X5'] + b6 * data.fit['X3'] * data.fit['X5']
+ggplot(data.fit, aes(x=X3, y=X5, z=inter)) + geom_contour_filled(bins=20)
 
 
 #print(xtable(fit_b, type = "latex"), file = "All Model Latex/MF_a_interact.tex")
